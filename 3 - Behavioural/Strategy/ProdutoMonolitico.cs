3846e2f1bd4074b24349769e654d6429c15df9a3
Lucas Rodrigues <lucas.chapola@gmail.com>
2017-03-01 22:35:18 -0300

    adicionado contra-exemplo ao padrao strategy

---
+++ 3 - Behavioural/Strategy/ProdutoMonolitico.cs
@@ -0,0 +1,32 @@
+﻿namespace Strategy
+{
+	public class ProdutoMonolitico
+	{
+		public double Preco { get; private set; }
+
+		public void DefinirPreco(int preco)
+		{
+			Preco = preco;
+		}
+
+		public void CalcularDesconto(Desconto tipoDesconto)
+		{
+			if (tipoDesconto == Desconto.Cartao)
+			{
+				// calcula desconto cartão
+				Preco *= 0.8;
+			}
+			if (tipoDesconto == Desconto.Boleto)
+			{
+				// calcula desconto boleto
+				Preco *= 0.9;
+			}
+		}
+	}
+
+	public enum Desconto
+	{
+		Cartao,
+		Boleto
+	}
+}
